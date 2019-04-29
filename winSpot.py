5ab517c62fc516749b3efb2718c5bdc8789144a9
Amine <amaayoufi@realytics.io>
2019-04-29 17:50:39 +0200

    Get only background files

--- winSpot.py
+++ winSpot.py
@@ -4,23 +4,39 @@ from PIL import Image
 
 foldername = "WinSpotFiles"
 
+
 def makeFolder():
-	cwd = os.getcwd()
-	dir = cwd + "\\" + foldername
-	if not os.path.exists(dir):
-		os.makedirs(dir)
+    cwd = os.getcwd()
+    dir = cwd + "\\" + foldername
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
 
 def main():
-	makeFolder()
-	homedir = os.environ['HOMEPATH']
-	dir = homedir + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets"
-	n = 0
-	for filename in os.listdir(dir):
-		cwd = os.getcwd()
-		with Image.open(filename) as img:
-			width, height = img.size
-		newfile = cwd + "\\" + str(foldername) + "\\" + str(n) + ".jpg"
-		shutil.copy(dir + "\\" + filename, newfile)
-		n += 1
+    makeFolder()
+    homedir = os.environ['HOMEPATH']
+    dir = homedir + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets"
+    n = 0
+    for filename in os.listdir(dir):
+        cwd = os.getcwd()
+
+        try:
+            with Image.open(dir + "\\" + filename) as img:
+                width, height = img.size
+        except:
+            continue
+
+        if height == 1080 and width == 1920:
+            print("test1")
+            newfile = cwd + "\\" + str(foldername) + "\\" + str(n) + ".jpg"
+        elif height == 1920 and width == 1080:
+            print("test2")
+            newfile = cwd + "\\" + str(foldername) + "\\" + str(n) + ".jpg"
+        else:
+            continue
+
+        shutil.copy(dir + "\\" + filename, newfile)
+        n += 1
+
 
 main()
